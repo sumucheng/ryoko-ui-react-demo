6f626f56afe510205900bd6cc7fcc724e853f6d9
kiki <772765729@qq.com>
2020-05-15 16:03:12 +0800

    修改代码展示主题

--- lib/demo/code/button-code.tsx
+++ lib/demo/code/button-code.tsx
@@ -5,19 +5,25 @@ interface Props {
     code: string
 }
 export default function (props: Props) {
+    console.log(defaultProps)
     return (
         <Highlight {...defaultProps} code={props.code} language="jsx">
-            {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre className={className} style={style}>
-                    {tokens.map((line, i) => (
-                        <div  {...getLineProps({ line, key: i })}>
-                            {line.map((token, key) => (
-                                <span {...getTokenProps({ token, key })}></span>
-                            ))}
-                        </div>
-                    ))}
-                </pre>
-            )}
+            {({ className, tokens, getLineProps, getTokenProps }) => {
+                const style = { background: "#fafafa" }
+                console.log(getTokenProps)
+                return (
+                    <pre className={className} style={style}>
+                        {tokens.map((line, i) => (
+                            <div  {...getLineProps({ line, key: i })}>
+                                {line.map((token, key) => (
+                                    <span {...getTokenProps({ token, key })}></span>
+                                ))}
+                            </div>
+                        ))}
+                    </pre>
+                )
+            }
+            }
 
         </Highlight>
 
